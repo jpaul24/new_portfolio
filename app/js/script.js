eb53280de2dc154929497d81776b24552ac32917
Joe Paul <joe.paul0712@gmail.com>
2019-11-07 16:55:21 +0000

    nav bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,15 @@
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+
+function fixNav() {
+  if(window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav')
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav')
+  }
+}
+
+window.addEventListener('scroll', fixNav);
